231a54189928c35e08a75db8c68d53e100e98780
Shmirsky <liorushomir@gmail.com>
2022-05-27 21:24:56 +0300

    updated app and dappstore_utils to work in remix compiler

--- contracts/app.sol
+++ contracts/app.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.8.0;
-
+pragma solidity >=0.4.22 <0.9.0;
+import {dappstore_utils} from './dappstore_utils.sol'; //import
 
 contract App {
     //App contract provides a data structure of an app and
@@ -17,22 +17,117 @@ contract App {
     uint price; //in wei
     uint RatingsNum;
 
-    bool owned; //will be filled by getter functions.
-    int myRating;
+    // bool owned; //will be filled by getter functions.
 
 
-    constructor(){
 
+    modifier validatePrice(uint _price){
+        require(_price > 0);
+        _;
     }
 
-    function update_app(uint app_id, string new_sha){
-
+    modifier validateString(string calldata _string){
+        require(check_if_string_empty(_string));
+        _;
     }
 
-    function publish_app(){
-
+    modifier validateOwner(){
+        require(msg.sender == creator);
+        _;
     }
+
+    function check_if_string_empty(string calldata _string)
+    pure private returns(bool){
+        return bytes(_string).length > 0;
+    }
+
+    function update_app_version(string calldata new_sha)
+    public validateOwner validateString(new_sha) {
+        fileSha256.push(new_sha);
+    }
+
+    function update_app_name(string calldata new_name)
+    private validateString(new_name){
+        name = new_name;
+    }
+
+    function update_app_description(string calldata new_description)
+    private validateString(new_description){
+        description = new_description;
+    }
+
+    function update_app_image(string calldata new_imgUrl)
+    private validateString(new_imgUrl){
+        imgUrl = new_imgUrl;
+    }
+
+    function update_app_info(string calldata new_name, string calldata new_description,
+                             string calldata new_imgUrl) external validateOwner{
+        if (check_if_string_empty(new_name)){
+            update_app_name(new_name);
+        }
+        if (check_if_string_empty(new_description)) {
+            update_app_description(new_description);
+        }
+        if (check_if_string_empty(new_imgUrl)){
+            update_app_image(new_imgUrl);
+        }
+    }
+
+    function update_app_price(uint new_price)
+    public validateOwner validatePrice(new_price){
+        price = new_price;
+    }
+
+    function download_app() external view returns (string memory){
+        return fileSha256[fileSha256.length - 1];
+    }
+
+    function get_app_rating() external view returns (int){
+        if (rating < 0){
+            return 0;
+        }
+        return rating;
+    }
+
+    function rate_app(int new_rating, bool first_rating, int old_rating) external {
+        require(new_rating > 0 && new_rating <=5, "illegal rating");
+        if (new_rating == dappstore_utils.UNRATED){
+            require(first_rating == true, "App has no previous ratings");
+            rating = new_rating;
+        } else if (first_rating == true){
+            rating = (rating * int(RatingsNum) + new_rating) / int(RatingsNum + 1);
+            RatingsNum = RatingsNum + 1;
+        } else {
+            require(old_rating > 0 && old_rating <=5, "illegal rating");
+            int rating_mid = (rating * int(RatingsNum) - old_rating) ;
+            rating = (rating_mid + new_rating) / int(RatingsNum);
+        }
+    }
+
+
+    constructor(uint256 _id, string memory _name, string memory _description,
+                string memory _magnetLink, string memory _imgUrl,
+                string memory _company, uint _price, string memory _fileSha256){
+        id  = _id;
+        name  = _name;
+        description = _description;
+        creator = payable(msg.sender);
+        fileSha256[0] = _fileSha256;
+        magnetLink = _magnetLink;
+        imgUrl = _imgUrl;
+        company = _company;
+        rating = dappstore_utils.UNRATED;
+        price = _price;
+        RatingsNum = 0;
+        // owned = _owned;
+//        myRating = _myRating;
+    }
+
+
 
 
 
 }
+
+
---
+++ contracts/dappstore_utils.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+library dappstore_utils {
+    string constant DEFAULT_APP_NAME = '';
+    string constant DEFAULT_DESCRIPTION_NAME = '';
+    string constant DEFAULT_EMPTY = '';
+    int constant UNRATED = -1;
+}
+
