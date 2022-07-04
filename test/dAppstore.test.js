54027fdb08052fd66e9d4b11f3e61ae5ac800301
Liorushomir <liorushomir@gmail.com>
2022-07-05 00:55:53 +0300

    updated tests

--- test/dAppstore.test.js
+++ test/dAppstore.test.js
@@ -10,9 +10,27 @@ contract('dAppStore', (accounts) => {
   let user_uploaded_apps = [];
   const user_address = accounts[0];
   let dAppStore;
-  // beforeEach(async function () {
-
-  // });
+  const expectAppInfo = (app, id, price = 8) =>{
+    console.assert(app.id.toNumber() == id, "app id is not correct");
+    console.assert(app.name == `name${id}`, "app name is not correct");
+    console.assert(app.description == `description${id}`, "app description is not correct");
+    console.assert(app.link == `link${id}`, "app link is not correct");
+    console.assert(app.img == `img${id}`, "app img is not correct");
+    console.assert(app.company == `company${id}`, "app company is not correct");
+    console.assert(app.price.toNumber() == price, "app price is not correct");
+    console.assert(app.category == `category${id}`, "app category is not correct");
+    console.assert(app.fileSha == `fileSha${id}`, "app fileSha is not correct");
+    
+    expect(app.id).to.equal(id.toString());
+    expect(app.name).to.equal(`name${id}`);
+    expect(app.description).to.equal(`description${id}`);
+    expect(app.link).to.equal(`link${id}`);
+    expect(app.img).to.equal(`img${id}`);
+    expect(app.company).to.equal(`company${id}`);
+    expect(app.price).to.equal(price.toString());
+    expect(app.category).to.equal(`category${id}`);
+    expect(app.fileSha).to.equal(`fileSha${id}`);
+}
 
   //Test case
   it('Should create a new dAppStore contract', async () => {
@@ -20,46 +38,72 @@ contract('dAppStore', (accounts) => {
     // console.log(`dAppStore address: ${dAppStore.address}`);
     // console.log(`dAppStore receipt: ${dAppStore.receipt}`);
     assert.isOk(dAppStore.address);
-  });  
-  // it('Create single app', async () => {
-  //   const name = "name" + `${0}`;
-  //   const description = "description";
-  //   const link = "link";
-  //   const img = "img";
-  //   const company = "company";
-  //   const price = 8;
-  //   const category = "category";
-  //   const fileSha = "fileSha0";
-  //   const user_address = accounts[0];
-  //   const app = await dAppStore.createNewApp(
-  //     name,
-  //     description,
-  //     link,
-  //     img,
-  //     company,
-  //     price,
-  //     category,
-  //     fileSha,
-  //     { from: user_address }
-  //   );
-  //   console.log(`app: ${app}`);
-  //   assert.isOk(app);
-  //   const app0 = await dAppStore.getAppBatch(0, 1);
-  //   console.log(`app0: ${app0}`);
+    const user_address = accounts[0];
+      for (let i = 0; i < x_apps; i++) {
+        console.log(`Creating app ${i}`);
+        const idx = `${i}`;
+        const name = "name" + idx;
+        const description = "description" + idx;
+        const link = "link" + idx;
+        const img = "img" + idx;
+        const company = "company" + idx;
+        const price = 8;
+        const category = "category" + idx;
+        const fileSha = "fileSha" + idx;
+        const app = await dAppStore.createNewApp(
+          name,
+          description,
+          link,
+          img,
+          company,
+          price,
+          category,
+          fileSha,
+          { from: user_address }
+        );
+      }
 
-  // });
+  });  
+//   it('Create single app', async () => {
+//     const name = "name" + `${0}`;
+//     const description = "description" + `${0}`;
+//     const link = "link" + `${0}`;
+//     const img = "img" + `${0}`;
+//     const company = "company" + `${0}`;
+//     const price = 8;
+//     const category = "category" + `${0}`;
+//     const fileSha = "fileSha0" + `${0}`;
+//     const user_address = accounts[0];
+//     const app = await dAppStore.createNewApp(
+//       name,
+//       description,
+//       link,
+//       img,
+//       company,
+//       price,
+//       category,
+//       fileSha,
+//       { from: user_address }
+//     );
+//     console.log(`app: ${app}`);
+//     assert.isOk(app);
+//     const app0 = await dAppStore.getAppBatch(0, 1);
+//     console.log(`app0: ${app0}`);
+
+//   });
   it(`Create ${x_apps} apps for same user`, async () => {
+    const initial_app_count = await dAppStore.getAppCount();
       const user_address = accounts[0];
       for (let i = 0; i < x_apps; i++) {
         console.log(`Creating app ${i}`);
         const idx = `${i}`;
         const name = "name" + idx;
-        const description = "description";
-        const link = "link";
-        const img = "img";
-        const company = "company";
+        const description = "description" + idx;
+        const link = "link" + idx;
+        const img = "img" + idx;
+        const company = "company" + idx;
         const price = 8;
-        const category = "category";
+        const category = "category" + idx;
         const fileSha = "fileSha" + idx;
         const app = await dAppStore.createNewApp(
           name,
@@ -75,37 +119,20 @@ contract('dAppStore', (accounts) => {
       }
       const apps = await dAppStore.getAppBatch(0, x_apps);
       // console.log(`apps: ${apps}`);
-      const apps_count = await dAppStore.getAppsCount();
-      expect(apps_count).to.be.equal(x_apps);
+      const apps_count = await dAppStore.getAppCount();
+      console.log(`apps_count: ${apps_count}`);
+      expect(apps_count.toNumber()).to.be.equal(x_apps  + initial_app_count.toNumber());
     });
-
   it('Should get first app', async () =>{
-    console.log('get first app');
     const app = await dAppStore.getAppBatch(0, 1);
-    console.log(app);
-    expect(app[0].id).to.equal(0);
-    expect(app[0].name).to.equal('name0');
-    expect(app[0].description).to.equal('description0');
-    expect(app[0].link).to.equal('link0');
-    expect(app[0].img).to.equal('img0');
-    expect(app[0].company).to.equal('company0');
-    expect(app[0].price).to.equal(8);
-    expect(app[0].category).to.equal('category0');
-    expect(app[0].fileSha).to.equal('fileSha0');
+    console.log(`first app: ${app}`);
+    expectAppInfo(app, 0);
+    
   });
   it('Should get last app', async () =>{
-    console.log('get last app');
     const app = await dAppStore.getAppBatch(x_apps-1, 1);
-    console.log(app);
-    expect(app[0].id).to.equal(x_apps-1);
-    expect(app[0].name).to.equal(`name${x_apps-1}`);
-    expect(app[0].description).to.equal(`description${x_apps-1}`);
-    expect(app[0].link).to.equal(`link${x_apps-1}`);
-    expect(app[0].img).to.equal(`img${x_apps-1}`);
-    expect(app[0].company).to.equal(`company${x_apps-1}`);
-    expect(app[0].price).to.equal(8);
-    expect(app[0].category).to.equal(`category${x_apps-1}`);
-    expect(app[0].fileSha).to.equal(`fileSha${x_apps-1}`);
+    console.log(`last app: ${app}`);
+    expectAppInfo(app, x_apps-1);
   });
   it(`Should get ${Math.ceil(x_apps/5)} apps from index 0 to  ${Math.ceil(x_apps/5)}`, async () => {
     const apps_num = Math.ceil(x_apps/5);
@@ -115,15 +142,7 @@ contract('dAppStore', (accounts) => {
     expect(appBatch.length).to.equal(apps_num);
 
     for (i = 0; i < apps_num; i++) {
-      expect(appBatch[i].id).to.equal(i);
-      expect(appBatch[i].name).to.equal(`name${i*5}`);
-      expect(appBatch[i].description).to.equal(`description${i*5}`);
-      expect(appBatch[i].link).to.equal(`link${i*5}`);
-      expect(appBatch[i].img).to.equal(`img${i*5}`);
-      expect(appBatch[i].company).to.equal(`company${i*5}`);
-      expect(appBatch[i].price).to.equal(8);
-      expect(appBatch[i].category).to.equal(`category${i*5}`);
-      expect(appBatch[i].fileSha).to.equal(`fileSha${i*5}`);
+        expectAppInfo(app, i);
     }
   });
   it (`Should get ${Math.ceil(x_apps/5)} apps from random index`, async () => {
@@ -135,20 +154,20 @@ contract('dAppStore', (accounts) => {
     expect(appBatch.length).to.equal(apps_num);
 
     for (i = 0; i < apps_num; i++) {
-      expect(appBatch[i].id).to.equal((i+rnd_idx) % x_apps);
+      expect(parseInt(appBatch[i].id)).to.equal((i+rnd_idx) % x_apps);
       expect(appBatch[i].name).to.equal(`name${(i+rnd_idx) % x_apps}`);
       expect(appBatch[i].description).to.equal(`description${(i+rnd_idx) % x_apps}`);
       expect(appBatch[i].link).to.equal(`link${(i+rnd_idx) % x_apps}`);
       expect(appBatch[i].img).to.equal(`img${(i+rnd_idx) % x_apps}`);
       expect(appBatch[i].company).to.equal(`company${(i+rnd_idx) % x_apps}`);
-      expect(appBatch[i].price).to.equal(8);
+      expect(parseInt(app[0].price)).to.equal(8);
       expect(appBatch[i].category).to.equal(`category${(i+rnd_idx) % x_apps}`);
       expect(appBatch[i].fileSha).to.equal(`fileSha${(i+rnd_idx) % x_apps}`);
       }
   });
   it('Get user published apps', async function () {
     const user_address = accounts[0];
-    const apps = await dAppStore.getUserApps(user_address);
+    const apps = await dAppStore.getPublishedAppsInfo(user_address);
     expect(apps.length).to.equal(user_uploaded_apps.length);
     const findAppByIdInArray = (id, appArray) => {
       for (let i = 0; i < appArray.length; i++) {
